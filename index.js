ac3423fdc9ab7a6543b57faa6ac768bb1c6ce9e8
Franklyn Rodriguez <franklynr92@gmail.com>
2021-07-01 14:15:13 -0400

    removing require and moving to passport

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys');
 const app = express();
 
 
