429dd1b8c73996eebf697dc5187411da96a7f257
Franklyn Rodriguez <franklynr92@gmail.com>
2021-07-01 13:17:51 -0400

    adding keys

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy'});
-});
+passport.use(new GoogleStrategy());
+// app.get('/', (req, res) => {
+//     res.send({ bye: 'buddy'});
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
