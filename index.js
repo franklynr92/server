5d84f62ce55279acc2471faf6702e4969a609104
Franklyn Rodriguez <franklynr92@gmail.com>
2021-07-01 14:07:35 -0400

    adding console log to see what data is coming through

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
   }, 
-  accessToken => {
-    console.log(accessToken);
+  (accessToken, refreshToken, profile, done) => {
+    console.log('access Token:', accessToken);
+    console.log('refresh Token:', refreshToken);
+    console.log('profile:', profile);
    }
   )
 );
