bf8812f9f633d32af6eaa4de559710830f1ead25
Franklyn Rodriguez <franklynr92@gmail.com>
2021-07-01 14:27:54 -0400

    requiring authRoutes file and calling it as well

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 //app object is defined here in index.js
 
 const app = express();
 
-
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
