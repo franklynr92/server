3d10e8debc4a1ee28773917967ea998d5e2fdcef
Franklyn Rodriguez <franklynr92@gmail.com>
2021-07-01 14:16:23 -0400

    adding to passport file and adding another dot for it to know to go up one directory go into config and find keys file

--- services/passport.js
+++ services/passport.js
@@ -1,3 +1,7 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('../config/keys');
+
 passport.use(
     new GoogleStrategy(
       {
