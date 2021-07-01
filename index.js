9eedf162a4710cd48fff2bb4115fa31fc10e8a6b
Franklyn Rodriguez <franklynr92@gmail.com>
2021-07-01 14:13:59 -0400

    organizing files and breaking up for readability

--- index.js
+++ index.js
@@ -4,29 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('./config/keys');
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-  }, 
-  (accessToken, refreshToken, profile, done) => {
-    console.log('access Token:', accessToken);
-    console.log('refresh Token:', refreshToken);
-    console.log('profile:', profile);
-   }
-  )
-);
 
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-    scope: ['profile', 'email'] 
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google')); //this route gives the code and exchanges it for the user profile
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/authRoutes.js
@@ -0,0 +1,8 @@
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+    scope: ['profile', 'email'] 
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google')); //this route gives the code and exchanges it for the user profile
