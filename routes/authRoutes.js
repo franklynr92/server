4208796d3abdcff56e87ba7d6b7add777eeeddb0
Franklyn Rodriguez <franklynr92@gmail.com>
2021-07-01 14:26:47 -0400

    exporting function from the file

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 const passport = require('passport');
 
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-    scope: ['profile', 'email'] 
-  })
-);
+module.exports = (app) => {
+    app.get(
+        '/auth/google', 
+        passport.authenticate('google', {
+        scope: ['profile', 'email'] 
+    })
+    );
+    //google has a list of different scopes or permissions we can read off a user's account
 
-app.get('/auth/google/callback', passport.authenticate('google')); //this route gives the code and exchanges it for the user profile
+    app.get('/auth/google/callback', passport.authenticate('google')); //this route gives the code and exchanges it for the user profile
+}
